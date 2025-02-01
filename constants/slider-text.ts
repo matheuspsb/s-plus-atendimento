a4d7814938738ca416d02a145cca72d2a7bf8818
Matheus Souza <matheusslg1@gmail.com>
2025-02-01 04:05:36 -0300

    style: change slider text

--- constants/slider-text.ts
+++ constants/slider-text.ts
@@ -2,15 +2,15 @@ import { AuthSliderProps } from "@/types/slider";
 
 export const sliderText: AuthSliderProps[] = [
   {
-      title: 'Components Based Design System',
-      description: 'Powerful and easy to use multipurpose theme'
+      title: 'Bem-vindo à S+ Atendimento!',
+      description: 'Seu atendimento faz a diferença! Cada interação é uma oportunidade de encantar o cliente.'
   },
   {
-      title: 'Ready to use components',
-      description: 'Ready made component to apply directly'
+      title: 'Lembre-se: Atendimento com empatia é a chave para um cliente feliz!',
+      description: 'Esteja sempre atento às necessidades do cliente. O suporte excepcional começa com você.'
   },
   {
-      title: 'Multiple dashboard and widgets',
-      description: '100+ widgets and customize controls'
+      title: 'Cada cliente é único, e você tem o poder de fazer a diferença.',
+      description: 'Seu sorriso e paciência são a melhor forma de ajudar nossos clientes.'
   }
 ];
